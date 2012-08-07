0f8eeb15aa80e338da788328f739e6603cc5e50c
Jasper Jongmans <jasper.jongmans@jongedemocraten.nl>
2012-08-07 11:28:35 +0200

    Remove cached settings.py

--- settings.py
+++
@@ -1,8 +0,0 @@
-from settings_default import *
-
-
-DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
-DATABASES['default']['NAME'] = 'db.sqlite'
-
-ADMIN_MEDIA_PREFIX = "/usr/local/lib/python2.7/site-packages/django/contrib/admin/media/"
-
