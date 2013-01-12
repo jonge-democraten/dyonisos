d2f30dc62f47ec084cc618bed6f1d5b54f6a9673
Bart Römgens <bart.romgens@gmail.com>
2013-01-12 18:28:27 +0100

    Do not check payment status if registration is older than 7 days. As specified by the iDeal doc, see ticket #646.

--- subscribe/management/commands/registrationcheck.py
+++ subscribe/management/commands/registrationcheck.py
@@ -1,10 +1,16 @@
-from django.core.management.base import BaseCommand, CommandError
-from subscribe.models import *
+from django.core.management.base import BaseCommand
+from subscribe.models import Registration
+import datetime
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        now = datetime.datetime.now()
+        days = 7
+        seconds = 0
+        
         # Get all registrations that are not payed, have a trixid and where 
-        # check_ttl > 0 to check their payment status.
+        # check_ttl > 0 to check their payment status.        
         for r in Registration.objects.filter(payed=False).filter(trxid__isnull=False).filter(check_ttl__gt=0):
-            r.check_payment_status()
-
+            # only check if the registration is not older than 7 days
+            if (now > r.registration_date + datetime.timedelta(days, seconds)):
+                r.check_payment_status()
