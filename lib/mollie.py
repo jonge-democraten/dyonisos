4994737d7f277380fc971685e3b06339cb0a2cf4
Floor Terra <floort@gmail.com>
2013-10-05 17:00:25 +0200

    Check arguments

--- lib/mollie.py
+++ lib/mollie.py
@@ -95,6 +95,24 @@ def banklist():
         yield bank
         
 def fetch(partnerid, amount, bank_id, description, reporturl, returnurl, profile_key=None):
+    if type(amount) == str:
+        if not amount.isdigit(): raise ValueError("Parameter amount should not contain non-digit characters.")
+        amount = int(amount)
+    elif type(amount) != int:
+        raise TypeError("Parameter amount should be a atring or an integer.")
+    if type(bank_id) == int:
+        # Bank_id should be a integer between 0 and 10000 with leading zeroes.
+        if bank_id < 0: raise ValueError("Parameter bank_id should be larger then 0.")
+        if bank_id >= 10000: raise ValueError("Parameter bank_id should be less than 10000.")
+        bank_id = "%04d" % (bank_id, )
+    elif type(bank_id) == str:
+        if not bank_id.isdigit(): raise ValueError("Parameter bank_id should contain only digits.")
+        if len(bank_id) != 4: raise ValueError("Parameter bank_id should contain exactly 4 digits (incl. leading zeroes).")
+    else:
+        raise TypeError("Parameter bank_id should be a integer or a string.")
+    if len(description) > 29:
+        raise ValueError("Parameter 'description' can't be longer than 29 characters.")
+    
     params = urllib.urlencode({
         "a": "fetch",
         "partnerid": partnerid,
