ef4eafbd2edf98dffda9118e907d5b35444cbb2e
Bart Romgens <bart.romgens@gmail.com>
2013-10-10 21:34:49 +0200

    Added south app for database migration.

--- settings_default.py
+++ settings_default.py
@@ -102,7 +102,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'subscribe',
+    'south', # for database migration
+    'subscribe', # the main app
 )
 
 
