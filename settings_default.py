4a6cc77e5ff42bdfd014908d2a3c7efa12ab7023
Bart Romgens <bart.romgens@gmail.com>
2013-10-24 00:14:03 +0200

    Added logging settings.

--- settings_default.py
+++ settings_default.py
@@ -106,6 +106,55 @@ INSTALLED_APPS = (
     'subscribe', # the main app
 )
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'logfile': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': INSTALL_DIR + "logfile",
+            'maxBytes': 5000000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+        'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'WARN',
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'subscribe': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+        },
+        'lib': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+        },
+    }
+}
 
 
 
