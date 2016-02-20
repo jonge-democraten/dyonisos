79587e70ba1ba26ef74d15d1ec13479deebfdb39
Tom van Dijk <tom@tvandijk.nl>
2016-02-20 23:49:32 +0100

    Improve local_settings.py example

--- events/local_settings_example.py
+++ events/local_settings_example.py
@@ -1,6 +1,7 @@
 # Dyonisos local settings file
 
 # SECURITY WARNING: Make this unique, and don't share it with anybody.
+# Fill it with some random characters and make it long and unique.
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -8,23 +9,24 @@ DEBUG = False
 
 ALLOWED_HOSTS = []
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'events',
-        'USER': 'events',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '',
-    }
-}
+# Set this to your favourite mysql database. Default: sqlite
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'events',
+#         'USER': 'events',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '',
+#     }
+# }
 
+# The STATIC_ROOT is not necessary for testing with 'manage.py runserver'
 # STATIC_ROOT = '/usr/share/events/static/'
 STATIC_URL = '/static/'
 
 # Janeus settings
 # Uncomment if you have an LDAP server
-
 # JANEUS_SERVER = "ldap://127.0.0.1:389/"
 # JANEUS_DN = "cn=readuser, ou=sysUsers, dc=jd, dc=nl"
 # JANEUS_PASS = ""
@@ -32,4 +34,6 @@ STATIC_URL = '/static/'
 
 # Key for Mollie
 # You need a Mollie account, use the test key for testing
+# This is only necessarry for testing paying. If you don't
+# have a Mollie account, then don't set prices.
 MOLLIE_KEY = ''
