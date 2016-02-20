a9b93bb53bc4ae3af0676f7b1abff6c66bd091bc
Tom van Dijk <tom@tvandijk.nl>
2016-02-20 17:50:58 +0100

    Some cleaning up and higher versions (preparing python 3)

--- events/local_settings_example.py
+++ events/local_settings_example.py
@@ -3,13 +3,9 @@
 # SECURITY WARNING: Make this unique, and don't share it with anybody.
 SECRET_KEY = ''
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-TEMPLATE_DEBUG = False
-
-ADMINS = (
-    ('name', 'name@domain.com'),
-)
 
 ALLOWED_HOSTS = ['events.jongedemocraten.nl']
 
@@ -24,20 +20,15 @@ DATABASES = {
     }
 }
 
-LANGUAGE_CODE = 'nl_NL'
-TIME_ZONE = 'Europe/Amsterdam'
+STATIC_ROOT = '/usr/share/events/static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = '/usr/share/events/htdocs/'
-STATIC_URL = 'https://events-static.jongedemocraten.nl/'
 
 # Janeus settings
 
 JANEUS_SERVER = "ldap://127.0.0.1:389/"
 JANEUS_DN = "cn=readuser, ou=sysUsers, dc=jd, dc=nl"
 JANEUS_PASS = ""
-JANEUS_AUTH = lambda user, groups: "role-team-ict" in groups or "role-bestuur-landelijk" in groups
-from django.db.models import Q
-JANEUS_AUTH_PERMISSIONS = lambda user,groups: Q(content_type__app_label='subscribe')
 AUTHENTICATION_BACKENDS = ('janeus.backend.JaneusBackend', 'django.contrib.auth.backends.ModelBackend',)
 
 
--- events/urls.py
+++ events/urls.py
@@ -1,19 +1,15 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from subscribe.views import HomeView
 
-urlpatterns = patterns('',
-    (r'^$', HomeView.as_view()),
-    (r'^inschrijven/(?P<slug>[\w-]+)/$', 'subscribe.views.register'), # Inschrijf formulier
+urlpatterns = [
+    url(r'^$', HomeView.as_view()),
+    url(r'^inschrijven/(?P<slug>[\w-]+)/$', 'subscribe.views.register'), # Inschrijf formulier
 
-    (r'^report/$', 'subscribe.views.check'), # this is the merchant return url
-    (r'^return/$', 'subscribe.views.return_page'), # this is the merchant return url
-    (r'^deleteEventQuestion/$', 'subscribe.views.delete_event_question'),
+    url(r'^report/$', 'subscribe.views.check'), # this is the merchant return url
+    url(r'^return/$', 'subscribe.views.return_page'), # this is the merchant return url
+    url(r'^deleteEventQuestion/$', 'subscribe.views.delete_event_question'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
-)
+    url(r'^admin/', include(admin.site.urls)),
+]
