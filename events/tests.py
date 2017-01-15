3c8fc3835bafaf338d982e7f96678e1bbc381ff7
Bart Römgens <bart.romgens@gmail.com>
2017-01-15 17:59:29 +0100

    Create tests for registration admin pages

--- events/tests.py
+++ events/tests.py
@@ -4,6 +4,7 @@ from django.test import Client
 from django.urls import reverse
 
 from subscribe.models import Event
+from subscribe.models import Registration
 from subscribe.forms import SubscribeForm
 
 
@@ -20,12 +21,14 @@ class TestUserPages(TestCase):
 
     def test_view_subscribe_page(self):
         events = Event.objects.all()
+        self.assertTrue(events.exists())
         for event in events:
             response = self.client.get(reverse('subscribe', kwargs={'slug': event.slug}))
             self.assertEqual(response.status_code, 200)
 
     def test_subscribe_form(self):
         events = Event.objects.all()
+        self.assertTrue(events.exists())
         for event in events:
             form_data = {
                 'first_name': 'John',
@@ -37,6 +40,7 @@ class TestUserPages(TestCase):
 
     def test_subscribe_form_empty_email(self):
         events = Event.objects.all()
+        self.assertTrue(events.exists())
         for event in events:
             form_data = {
                 'first_name': '',
@@ -45,7 +49,6 @@ class TestUserPages(TestCase):
             }
             form = SubscribeForm(event=event, data=form_data)
             self.assertFalse(form.is_valid())
-
 
 
 class TestCaseAdminLogin(TestCase):
@@ -67,8 +70,26 @@ class TestCaseAdminLogin(TestCase):
 
 class TestAdminPages(TestCaseAdminLogin):
 
+    def test_events_page(self):
+        response = self.client.get('/admin/subscribe/event/')
+        self.assertEqual(response.status_code, 200)
+
     def test_event_edit_pages(self):
         events = Event.objects.all()
+        self.assertTrue(events.exists())
         for event in events:
             response = self.client.get('/admin/subscribe/event/' + str(event.id) + '/change/')
             self.assertEqual(response.status_code, 200)
+
+    def test_registrations_page(self):
+        response = self.client.get('/admin/subscribe/registration/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_registration_page(self):
+        registrations = Registration.objects.all()
+        self.assertTrue(registrations.exists())
+        for registration in registrations:
+            response = self.client.get('/admin/subscribe/registration/' + str(registration.id) + '/change/')
+            self.assertEqual(response.status_code, 200)
+
+
