a95df9cfcad8aad28cec1029be10b6d9dd05b088
Bart Römgens <bart.romgens@gmail.com>
2017-01-15 17:46:08 +0100

    Create tests for homepage, subscribe page and form

--- events/tests.py
+++ events/tests.py
@@ -1,12 +1,55 @@
 from django.conf import settings
 from django.test import TestCase
 from django.test import Client
+from django.urls import reverse
 
 from subscribe.models import Event
+from subscribe.forms import SubscribeForm
+
+
+class TestUserPages(TestCase):
+    """Test case with client for pages for normal user"""
+    fixtures = ['test_data.json']
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_view_homepage(self):
+        response = self.client.get(reverse('homepage'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_subscribe_page(self):
+        events = Event.objects.all()
+        for event in events:
+            response = self.client.get(reverse('subscribe', kwargs={'slug': event.slug}))
+            self.assertEqual(response.status_code, 200)
+
+    def test_subscribe_form(self):
+        events = Event.objects.all()
+        for event in events:
+            form_data = {
+                'first_name': 'John',
+                'last_name': 'Doe',
+                'email': 'john@doe.com',
+            }
+            form = SubscribeForm(event=event, data=form_data)
+            self.assertTrue(form.is_valid())
+
+    def test_subscribe_form_empty_email(self):
+        events = Event.objects.all()
+        for event in events:
+            form_data = {
+                'first_name': '',
+                'last_name': '',
+                'email': 'john@doe.com',
+            }
+            form = SubscribeForm(event=event, data=form_data)
+            self.assertFalse(form.is_valid())
+
 
 
 class TestCaseAdminLogin(TestCase):
-    """Test case with client and login as admin function."""
+    """Test case with client and login as admin function"""
     fixtures = ['test_data.json']
 
     def setUp(self):
