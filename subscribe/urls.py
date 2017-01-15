36716d286c054bcae84dfc0e60ed03bdbbf7c354
Bart Römgens <bart.romgens@gmail.com>
2017-01-15 17:45:35 +0100

    create reverse url names

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -3,9 +3,9 @@ from subscribe import views
 
 
 urlpatterns = [
-    url(r'^$', views.HomeView.as_view()),
-    url(r'^inschrijven/(?P<slug>[\w-]+)/$', views.register),
-    url(r'^deleteEventQuestion/$', views.delete_event_question),
+    url(r'^$', views.HomeView.as_view(), name='homepage'),
+    url(r'^inschrijven/(?P<slug>[\w-]+)/$', views.register, name='subscribe'),
+    url(r'^deleteEventQuestion/$', views.delete_event_question, name='delete-event-question'),
     url(r'^webhook/(?P<id>\d+)/?$', views.webhook, name='webhook'),
     url(r'^return/(?P<id>\d+)/?$', views.return_page, name='return_page'),
 ]
