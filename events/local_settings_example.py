70c6a4303387b71ef414ea45a6d0b525480fbd19
Bart Römgens <bart.romgens@gmail.com>
2017-01-15 13:18:51 +0100

    Add localhost to ALLOWED_HOSTS in local settings example

--- events/local_settings_example.py
+++ events/local_settings_example.py
@@ -7,7 +7,7 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Set this to your favourite mysql database. Default: sqlite
 # DATABASES = {
